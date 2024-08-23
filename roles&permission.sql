fdb46cc2bf1b7c7422767675fcca754bb679a67f
justmogen <justmogen@gmail.com>
2024-08-23 08:44:50 +0300

    modified query, will include roles later

--- roles&permission.sql
+++ roles&permission.sql
@@ -1,4 +1,4 @@
 -- DROP DATABASE IF EXISTS countryclub WITH (FORCE);
 
-REASSIGN OWNED BY target_role TO new_owner;
+-- REASSIGN OWNED BY target_role TO new_owner;
 -- DROP OWNED BY [name]; --drops owned objects
